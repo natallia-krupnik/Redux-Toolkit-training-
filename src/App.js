844af4daba04154358db4fd8959634288ae8ebf5
Natallia Krupnik <natalliakrup@gmail.com>
2022-09-05 08:52:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment, incrementByAmount, decrementByAmount, multiByAmount } from './redux/counter'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+	const { count } = useSelector((state) => state.counter)
+	const dispatch = useDispatch()
+
+	return (
+		<div className="App">
+			<p>Score: {count}</p>
+			<button onClick={() => dispatch(increment())}>+</button>
+			<button onClick={() => dispatch(decrement())}>-</button>
+			<button onClick={() => dispatch(incrementByAmount(5))}>+5</button>
+			<button onClick={() => dispatch(decrementByAmount(5))}>-5</button>
+			<button onClick={() => dispatch(multiByAmount(5))}>*5</button>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counter from './counter'
+
+export const store = configureStore({
+	reducer: {
+		counter: counter
+		//or simple counter 
+	},
+})
